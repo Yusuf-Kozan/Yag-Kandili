1d8d6dd8d451c7347ce4ff669ad55e5f3f91ffc5
My Name <my-name@chromium.org>
2020-07-23 15:41:13 +0300

    Kilnevüg kütüphanesine Girdiler sınıfı ve metinUygun fonksiyonu eklendi.

--- Data/GirdiDenetle.cs
+++ Data/GirdiDenetle.cs
@@ -2,5 +2,23 @@ using System;
 
 namespace Kilnevüg
 {
-    
+    class Girdiler
+    {
+        public bool metinUygun(string metin)
+        {
+            char[] mtn = metin.ToCharArray();
+            int hataSayısı = 0;
+            for (int i = 0; i < mtn.Length; i++)
+            {
+                if ( (mtn[i] == ';') || (mtn[i] == '(') || (mtn[i] == ')') )
+                {
+                    hataSayısı++;
+                    return false;
+                }
+            }
+            if (hataSayısı == 0)
+                return true;
+            return false;
+        }
+    }
 }
