f11d342f90ff7ac03838b0df7ae911cd246d1689
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2020-08-19 00:57:22 +0300

    Kullanıcılara verilecek eşsiz kimlikler için Kilnevüg kütüphanesi altında Kullanıcı_Kimliği yapısı oluşturuldu.

---
+++ Data/Kimlikler.cs
@@ -0,0 +1,13 @@
+using System;
+using Esas;
+
+namespace Kilnevüg
+{
+    public struct Kullanıcı_Kimliği
+    {
+        public string kullanıcı_adı;
+        public string üstünlük;
+        public DateTime başlangıç;
+        public string dosya_yolu;
+    }
+}
