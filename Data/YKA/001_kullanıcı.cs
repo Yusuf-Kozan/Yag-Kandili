bf5b4664aa9b220c62ab15d7f35a384b62f8468f
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2020-08-20 00:05:35 +0300

    Yağ Kandili Arşivi kütüphanesi oluşturulmaya başlandı ve kullanıcı oluşturma fonksiyonu yazıldı.

---
+++ Data/YKA/001_kullanıcı.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using Esas;
+using Kilnevüg;
+
+namespace YK_Arşiv
+{
+    public class kullanıcı
+    {
+        public void Oluştur(çÜye üye, string kimlik)
+        {
+            string[] belge = new string[] {
+                "YKA+0+001",
+                "",
+                kimlik,
+                "Kullanıcı_Adı:"+üye.KULLANICI_ADI,
+                "Ad:"+üye.AD,
+                "Soyadı:"+üye.SOYADI,
+                "E_Posta:"+üye.E_POSTA,
+                "Başlangıç:"+üye.BAŞLANGIÇ.ToString(),
+                "Resim:"
+            };
+            string dosyaAdı = üye.KULLANICI_ADI + ".yka";
+            string esasYol = System.IO.Path.Combine(üye.DizinYolu(), dosyaAdı);
+            System.IO.File.WriteAllLines(esasYol, belge);
+        }
+    }
+}
