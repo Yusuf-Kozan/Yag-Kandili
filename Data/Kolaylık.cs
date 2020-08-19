6605302fc650de88398e9988535c83e9ada0187d
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2020-08-19 17:10:41 +0300

    İşlemler kütüphanesi oluşturuldu ve yeni üye oluşturma kolaylaştırıldı.

---
+++ Data/Kolaylık.cs
@@ -0,0 +1,16 @@
+using System;
+using Esas;
+using Kilnevüg;
+
+namespace İşlemler
+{
+    public class Oturumİşlemleri
+    {
+        public void Üye_Oluştur(çÜye üye)
+        {
+            Kullanıcı_Kimliği kimlik = new Kullanıcı_Kimliği(üye);
+            TabanlıVeri.Üye_Ekle(üye, kimlik.Kimlik);
+            TabanlıVeri.Kullanıcı_Dizini_Oluştur(üye);
+        }
+    }
+}
