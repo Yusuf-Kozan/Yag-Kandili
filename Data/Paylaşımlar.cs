d28fb30a25e0458468e3ce564756f5c1929922c7
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2020-12-19 00:51:58 +0300

    Yeni paylaşım biçimine uyduruldu.

--- Data/Paylaşımlar.cs
+++ Data/Paylaşımlar.cs
@@ -3,52 +3,47 @@ using Kilnevüg;
 
 namespace Esas
 {
-    public class yazıPaylaş
+    public class Paylaşım
     {
-        public string BAŞLIK;
-        public string İÇERİK;
-        public string PAYLAŞAN;
-        public string KİLMİK;
-        public DateTime TARİH;
+        public long KİMLİK_1 {get; set;} // Veri tabanı tarafından otomatik artırılan sayılı kimlik
+        public string KİMLİK_2 {get; set;} // Yağ Kandili'nin oluşturmaya çalışacağı eşsizimsi kimlik
+        public string BAŞLIK {get; set;} // Paylaşımın başlığı
+        public string İÇERİK {get; set;} // Paylaşımın içerik kısmı (Asıl olay burada dönecek.)
+        public string EKLENTİ {get; set;} // Paylaşıma yerleştirilecek köprü, resim, video vb. şeyler
+        public string PAYLAŞAN {get; set;} // Paylaşımı yapan kişinin kullanıcı kimliği
+        public string OTURUM {get; set;} // Paylaşımın yapıldığı oturumun kimliği
+        public DateTime TARİH {get; set;} // Paylaşımın yapıldığı tarih
 
-        public yazıPaylaş() { }
-
-        public yazıPaylaş(string başlık, string içerik, string paylaşan, string kilmik, DateTime tarih)
+        public Paylaşım (long kimlik1, string kimlik2, string başlık, string içerik, string eklenti,
+                        string paylaşan, string oturum, DateTime tarih)
         {
-            BAŞLIK = başlık;
-            İÇERİK = içerik;
-            PAYLAŞAN = paylaşan;
-            KİLMİK = kilmik;
-            TARİH = tarih;
-        }
-
-        public yazıPaylaş(yazpay pay)
-        {
-            BAŞLIK = pay.BAŞLIK;
-            İÇERİK = pay.İÇERİK;
-            PAYLAŞAN = pay.PAYLAŞAN;
-            KİLMİK = pay.KİLMİK;
-            TARİH = pay.TARİH;
-        }
-
-        public yazpay ToYazpay() {
-            yazpay a;
-            a.BAŞLIK = BAŞLIK;
-            a.İÇERİK = İÇERİK;
-            a.PAYLAŞAN = PAYLAŞAN;
-            a.KİLMİK = KİLMİK;
-            a.TARİH = TARİH;
-
-            return a;
+                KİMLİK_1 = kimlik1;
+                KİMLİK_2 = kimlik2;
+                BAŞLIK = başlık;
+                İÇERİK = içerik;
+                EKLENTİ = eklenti;
+                PAYLAŞAN = paylaşan;
+                OTURUM = oturum;
+                TARİH = tarih;
         }
     }
-
-    public struct yazpay
+    public struct paylaşım
     {
-        public string BAŞLIK;
-        public string İÇERİK;
-        public string PAYLAŞAN;
-        public string KİLMİK;
+        public long KİMLİK_1;
+        public string KİMLİK_2, BAŞLIK, İÇERİK, EKLENTİ, PAYLAŞAN, OTURUM;
         public DateTime TARİH;
+
+        public paylaşım (long kimlik1, string kimlik2, string başlık, string içerik, string eklenti,
+                        string paylaşan, string oturum, DateTime tarih)
+        {
+                KİMLİK_1 = kimlik1;
+                KİMLİK_2 = kimlik2;
+                BAŞLIK = başlık;
+                İÇERİK = içerik;
+                EKLENTİ = eklenti;
+                PAYLAŞAN = paylaşan;
+                OTURUM = oturum;
+                TARİH = tarih;
+        }
     }
 }
