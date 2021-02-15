207ae9e7a6a0f834e0cb655f03a8c13da123af27
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2021-02-16 00:22:31 +0300

    Parola karma fonksiyonu güçlendirildi. Ayrıca adı anlaşılır biçime getirildi.

--- Data/Güvenlik/Parolalar.cs
+++ Data/Güvenlik/Parolalar.cs
@@ -2,14 +2,14 @@ namespace Kilnevüg
 {
     public static class Parolalar
     {
-        public static string azDahaGüvenliParola(string parola, string tuz)
+        public static string ar2id4_32_2_32(string parola, string tuz)
         {
             //Argon2id ile parola karma fonksiyonu
-            // 4 tekrar, 16 MB bellek, 1 eş zamanlı işlem, 32 B çıktı
+            // 4 tekrar, 32 MiB bellek, 2 eş zamanlı işlem, 32 B çıktı
             byte[] bParola = System.Text.Encoding.Unicode.GetBytes(parola);
             byte[] bTuz = System.Text.Encoding.Unicode.GetBytes(tuz);
             CryptHash.Net.Hash.Argon2id argon2id = new CryptHash.Net.Hash.Argon2id();
-            string sonuç = argon2id.ComputeHash(bParola, 4, (16*1024), 1, 32, bTuz).HashString;
+            string sonuç = argon2id.ComputeHash(bParola, 4, (32*1024), 2, 32, bTuz).HashString;
             return sonuç;
         }
     }
