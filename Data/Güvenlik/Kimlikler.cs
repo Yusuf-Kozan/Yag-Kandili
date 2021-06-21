321ad7bbe4e1028baaaf68e873c0d549f3ca6fb2
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2021-06-21 21:26:13 +0300

    Kullanıcı kimlikleri yenilendi.

--- Data/Güvenlik/Kimlikler.cs
+++ Data/Güvenlik/Kimlikler.cs
@@ -19,70 +19,34 @@ namespace Kilnevüg
         public static string Kimlik2(Paylaşım paylaşım)
         {
             CryptHash.Net.Hash.Hash.SHA512 sha512 = new CryptHash.Net.Hash.Hash.SHA512();
-            string zaman = paylaşım.TARİH.ToString("ddMMyyyyHHmmss");
+            string zaman = paylaşım.TARİH.ToString("yyyyMMddHHmmss");
             string karılmamış = paylaşım.BAŞLIK + paylaşım.İÇERİK + paylaşım.PAYLAŞAN + paylaşım.OTURUM + zaman;
             return sha512.ComputeHash(karılmamış).HashString;
         }
         public static string Kimlik2(string başlık, string içerik, string paylaşan, string oturum, DateTime tarih)
         {
             CryptHash.Net.Hash.Hash.SHA512 sha512 = new CryptHash.Net.Hash.Hash.SHA512();
-            string zaman = tarih.ToString("ddMMyyyyHHmmss");
+            string zaman = tarih.ToString("yyyyMMddHHmmss");
             string karılmamış = başlık + içerik + paylaşan + oturum + zaman;
             return sha512.ComputeHash(karılmamış).HashString;
         }
     }
-    public struct Kullanıcı_Kimliği
+    public class KullanıcıKimliği
     {
-        public string Kullanıcı_Adı;
-        public string Üstünlük;
-        public DateTime Başlangıç;
-        public string Dosya_Yolu;
-        public string Kimlik;
-        public Kullanıcı_Kimliği(string kullanıcı_adı, string üstünlük, DateTime başlangıç, string dosya_yolu)
+        public static string Kimlik(string kullanıcı_adı, string ad, string soyadı,
+                                    string e_posta, DateTime başlangıç_tarihi)
         {
-            this.Kullanıcı_Adı = kullanıcı_adı;
-            this.Üstünlük = üstünlük;
-            this.Başlangıç = başlangıç;
-            this.Dosya_Yolu = dosya_yolu;
-            this.Kimlik = Kullanıcı_Kimliği_Hatalar.kimlikYap(kullanıcı_adı, üstünlük, başlangıç, dosya_yolu);
+            CryptHash.Net.Hash.Hash.SHA512 sha512 = new CryptHash.Net.Hash.Hash.SHA512();
+            string tarih = başlangıç_tarihi.ToString("yyyyMMddHHmmss");
+            string karılmamış = kullanıcı_adı + ad + tarih + soyadı + e_posta;
+            return sha512.ComputeHash(karılmamış).HashString;
         }
-        public Kullanıcı_Kimliği(çÜye üye)
+        public static string Kimlik(Üye üye)
         {
-            this.Kullanıcı_Adı = üye.KULLANICI_ADI;
-            this.Üstünlük = üye.ÜSTÜNLÜK;
-            this.Başlangıç = üye.BAŞLANGIÇ;
-            this.Dosya_Yolu = üye.DizinYolu();
-            this.Kimlik = Kullanıcı_Kimliği_Hatalar.kimlikYap(üye.KULLANICI_ADI, üye.ÜSTÜNLÜK, üye.BAŞLANGIÇ, üye.DizinYolu());
-        }
-    }
-    public class Kullanıcı_Kimliği_Hatalar
-    {
-        public static string kimlikYap(string kullanıcı_adı, string üstünlük, DateTime başlangıç, string dosya_yolu)
-        {
-            string üst = null;
-            if (üstünlük == "kullanıcı") {üst = "k";} else if (üstünlük == "yönetici") {üst = "y";} else {üst = "a";}
-            string yeniKullanıcıKimliği = rastgele.sayılar(8).ToString() + "(" + kullanıcı_adı + rastgele.harfler(5) + üst
-            + ")" + başlangıç.ToString().Replace(" ","") + rastgele.sayılar(16).ToString() + rastgele.harfler(3);
-            return yeniKullanıcıKimliği;
-        }
-    }
-    public struct rastgele
-    {
-        public static string harfler(int sayı)
-        {
-            string abc = "abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ";
-            Random random = new Random();
-            string sonuç = null;
-            for (int i = 0; i < sayı; i++)
-            {
-                sonuç += abc[random.Next(0,57)];   
-            }
-            return sonuç;
-        }
-        public static int sayılar(int bölen)
-        {
-            Random random = new Random();
-            return random.Next(0 ,(int.MaxValue/bölen));
+            CryptHash.Net.Hash.Hash.SHA512 sha512 = new CryptHash.Net.Hash.Hash.SHA512();
+            string tarih = üye.BAŞLANGIÇ.ToString("yyyyMMddHHmmss");
+            string karılmamış = üye.KULLANICI_ADI + üye.AD + tarih + üye.SOYADI + üye.E_POSTA;
+            return sha512.ComputeHash(karılmamış).HashString;
         }
     }
 }
