2cf16dd6a141778dfb04b00606bd00d85c03be96
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2021-09-15 22:41:50 +0300

    Takip edilenlerin paylaşımları artık okunabiliyor

--- Data/VeriTabanı/Paylaşım.cs
+++ Data/VeriTabanı/Paylaşım.cs
@@ -185,6 +185,87 @@ namespace Esas.VeriTabanı
 
             return paylaşımlar;
         }
+        public static string[,][] TakipEdilenlerinBilgileriylePaylaşımları(string[,] takip_edilenler)
+        {
+            // a sıra sayısı olursa
+            // takip_edilenler[a, 0] = kullanıcı kimliği, takip_edilenler[a, 1] = takip düzeyi
+            
+            // [a, 0][] paylaşım bilgileri, [a, 1][] paylaşan bilgileri
+            string SorguİçinKimlikler = "";
+            for (long i = 0; i < takip_edilenler.GetLongLength(0); i++)
+            {
+                SorguİçinKimlikler += $"@takip{i.ToString()}, ";
+            }
+            SorguİçinKimlikler = $"({SorguİçinKimlikler.Substring(0,(SorguİçinKimlikler.Length-2))})";
+            string komut_metni = $"SELECT COUNT(Kimlik2) FROM {Paylaşım.TabloAdı()} " +
+                                $"INNER JOIN {Üyelik.TabloAdı()} " +
+                                $"ON {Paylaşım.TabloAdı()}.Paylaşan = {Üyelik.TabloAdı()}.Kimlik " +
+                                "WHERE Eklenti NOT LIKE '%>gizli%' AND" + 
+                                $" Paylaşan IN {SorguİçinKimlikler};";
+            MySqlConnection bağlantı = new MySqlConnection(Bağlantı.bağlantı_dizesi);
+            bağlantı.Open();
+            MySqlCommand komut = new MySqlCommand(komut_metni, bağlantı);
+            for (int j = 0; j < takip_edilenler.GetLongLength(0); j++)
+            {
+                komut.Parameters.AddWithValue($"@takip{j.ToString()}", takip_edilenler[j,0]);
+            }
+            int paylaşım_niceliği = int.Parse(komut.ExecuteScalar().ToString());
+            komut.Dispose();
+            string[,][] paylaşımlar;
+
+            if (paylaşım_niceliği < 1)
+            {
+                paylaşımlar = new string[0,0][];
+                bağlantı.Close(); bağlantı.Dispose();
+                return paylaşımlar;
+            }
+
+            komut_metni = $"SELECT * FROM {Paylaşım.TabloAdı()} " +
+                        $"INNER JOIN {Üyelik.TabloAdı()} " +
+                        $"ON {Paylaşım.TabloAdı()}.Paylaşan = {Üyelik.TabloAdı()}.Kimlik " +
+                        "WHERE Eklenti NOT LIKE '%>gizli%' AND" + 
+                        $" Paylaşan IN {SorguİçinKimlikler} " +
+                        "ORDER BY Tarih DESC, Kimlik1 DESC;";
+            komut = new MySqlCommand(komut_metni, bağlantı);
+            for (int j = 0; j < takip_edilenler.GetLongLength(0); j++)
+            {
+                komut.Parameters.AddWithValue($"@takip{j.ToString()}", takip_edilenler[j,0]);
+            }
+            MySqlDataReader veri_okuyucu = komut.ExecuteReader();
+            int döngü_turu = 0;
+            paylaşımlar = new string[paylaşım_niceliği, 2][];
+            while (veri_okuyucu.Read())
+            {
+                paylaşımlar[döngü_turu, 0] = new string[9]; //paylaşım bilgileri
+                paylaşımlar[döngü_turu, 1] = new string[8]; //paylaşan bilgileri
+
+                paylaşımlar[döngü_turu, 0][0] = veri_okuyucu["Kimlik1"].ToString();
+                paylaşımlar[döngü_turu, 0][1] = veri_okuyucu["Kimlik2"].ToString();
+                paylaşımlar[döngü_turu, 0][2] = veri_okuyucu["Başlık"].ToString();
+                paylaşımlar[döngü_turu, 0][3] = veri_okuyucu["İçerik"].ToString();
+                paylaşımlar[döngü_turu, 0][4] = veri_okuyucu["Eklenti"].ToString();
+                paylaşımlar[döngü_turu, 0][5] = veri_okuyucu["Paylaşan"].ToString();
+                paylaşımlar[döngü_turu, 0][6] = veri_okuyucu["Oturum"].ToString();
+                paylaşımlar[döngü_turu, 0][7] = veri_okuyucu["Tarih"].ToString();
+                paylaşımlar[döngü_turu, 0][8] = veri_okuyucu["Lisans"].ToString();
+
+                paylaşımlar[döngü_turu, 1][0] = veri_okuyucu["Ad"].ToString();
+                paylaşımlar[döngü_turu, 1][1] = veri_okuyucu["Soyadı"].ToString();
+                paylaşımlar[döngü_turu, 1][2] = veri_okuyucu["Kullanıcı_Adı"].ToString();
+                paylaşımlar[döngü_turu, 1][3] = veri_okuyucu["E_Posta"].ToString();
+                paylaşımlar[döngü_turu, 1][4] = veri_okuyucu["Üstünlük"].ToString();
+                paylaşımlar[döngü_turu, 1][5] = veri_okuyucu["Başlangıç"].ToString();
+                paylaşımlar[döngü_turu, 1][6] = veri_okuyucu["Resim"].ToString();
+                paylaşımlar[döngü_turu, 1][7] = veri_okuyucu["Kimlik"].ToString();
+
+                döngü_turu++;
+            }
+            veri_okuyucu.Close(); veri_okuyucu.Dispose();
+            komut.Dispose();
+            bağlantı.Close(); bağlantı.Dispose();
+
+            return paylaşımlar;
+        }
         public static paylaşım[] KişininTümAçıkPaylaşımları(string kullanıcı_kimliği)
         {
             string komut_metni = $"SELECT COUNT(Kimlik1) FROM {TabloAdı()} " +
