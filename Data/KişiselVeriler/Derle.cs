a4d95aad7e04e32b6f7e406edccf1801d4634781
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2022-02-01 20:18:28 +0300

    Kişisel verileri derleme kısmına başlanacak.

---
+++ Data/KişiselVeriler/Derle.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+
+namespace Esas.KişiselVeriler
+{
+    public class Derleyici
+    {
+        // Bu kısım henüz tasarım aşamasında.
+        // Gelecekte yapılacak.
+    }
+}
