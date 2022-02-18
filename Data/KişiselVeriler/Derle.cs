0c0033b292c9f992a5edeac158ac37e30bd1a2f9
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2022-02-19 00:26:58 +0300

    veri_derlemesi yapısı oluşturuldu.

--- Data/KişiselVeriler/Derle.cs
+++ Data/KişiselVeriler/Derle.cs
@@ -1,11 +1,27 @@
 using System;
 using System.IO;
+using Esas.VeriTabanı;
 
 namespace Esas.KişiselVeriler
 {
-    public class Derleyici
+    internal struct veri_derlemesi
     {
-        // Bu kısım henüz tasarım aşamasında.
-        // Gelecekte yapılacak.
+        parolasız_üye ÜYELİK_BİLGİLERİ;
+        paylaşım[] YAPTIĞI_PAYLAŞIMLAR;
+        değerli_paylaşım[] DEĞERLENDİRDİĞİ_PAYLAŞIMLAR;
+        köklü_söz[] SÖZLERİ;
+        takip[] TAKİP_ETTİĞİ_KİŞİLER;
+        takip[] TAKİP_ETTİĞİ_SÖYLEŞİLER;
+
+        internal veri_derlemesi(string kullanıcı_kimliği)
+        {
+            ÜYELİK_BİLGİLERİ = Üyelik.ParolasızÜyeBilgileri(kullanıcı_kimliği);
+            YAPTIĞI_PAYLAŞIMLAR = VeriTabanı.Paylaşım.KişininTümPaylaşımları(kullanıcı_kimliği);
+            DEĞERLENDİRDİĞİ_PAYLAŞIMLAR = 
+                        VeriTabanı.Beğeni.KişininDeğerlendirdiğiPaylaşımlar(kullanıcı_kimliği);
+            SÖZLERİ = Söyleşi.KişininSöyledikleri(kullanıcı_kimliği);
+            TAKİP_ETTİĞİ_KİŞİLER = VeriTabanı.Takip.TakipEdilenKullanıcılar(kullanıcı_kimliği);
+            TAKİP_ETTİĞİ_SÖYLEŞİLER = VeriTabanı.Takip.TakipEdilenSöyleşiler(kullanıcı_kimliği);
+        }
     }
 }
