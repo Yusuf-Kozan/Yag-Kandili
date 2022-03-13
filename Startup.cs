8544a5fbc369dc1bd8053715988e381cf8c2af55
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2022-03-13 19:05:13 +0300

    BlazorDownloadFile hizmeti eklendi.

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BlazorDownloadFile;
 
 namespace YağKandili
 {
@@ -28,6 +29,7 @@ namespace YağKandili
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddHttpContextAccessor();
+            services.AddBlazorDownloadFile(ServiceLifetime.Scoped);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
